fe78f0374166f2c356f001f98ba877b2838ac1c1
adrienphan <adrienphancaophai@gmail.com>
2021-12-20 11:10:27 +0100

    ajout de la classe server

---
+++ Bataille navale/Server.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Bataille_navale
+{
+    internal class Server
+    {
+        public static void OpenServer()
+        {
+            TcpListener server = null;
+            try
+            {
+                // Création des variables contenant le port et l'adresse IP que le server doit écouter.
+                Int32 port = 13000;
+                IPAddress localAddr = IPAddress.Parse("192.168.1.67");
+
+                // Création d'un objet TcpListener avec en paramétre le port et l'adresse IP
+                server = new TcpListener(localAddr, port);
+
+                // Ouverture du server.
+                server.Start();
+
+                // Création d'une variable bytes pour récupérer les données transmise et d'une variable string pour récupérer les données transcrit en string 
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                // Lancement de la boucle d'écoute.
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    // Reception de la requête demande client envoyé..
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    data = null;
+
+                    // Lecture du contenu envoyé
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    // Boucle sur le contenu envoyé.
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // transcie les bytes envoyé en string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
+
+                        // Mise en majuscule du message reçu.
+                        data = data.ToUpper();
+
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                        // renvoie le message.
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine("Sent: {0}", data);
+                    }
+
+                    // Shutdown and end connection
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+        }
+    }
+}
