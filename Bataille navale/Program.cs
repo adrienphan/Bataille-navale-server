dcd9e33cc343e40b890951cbf7e60e633060ec77
RomRav <romain.ravault@gmail.com>
2021-12-21 09:38:50 +0100

    Ajout du server

--- Bataille navale/Program.cs
+++ Bataille navale/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Net.Sockets;
 using System.Text.RegularExpressions;
 using Bataille_navale;
+using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
@@ -12,25 +13,38 @@ namespace ConsoleApplication1
     {
         public static int longitudeShot = 0;
         public static int latitudeShot = 0;
-        static void Main(string[] args)
+        public static string[,] battleMap = new string[10, 10];
+        public static Dictionary<string, int> boats = new Dictionary<string, int>();
+        public static string message = "";
+        public static string linePosition = "";
+        public static string columnPosition = ";";
+        public static string playerInput = "";
+        public static int playerHealth;
+        public static void Main(string[] args)
         {
-            //Ouverture du serveur
+            Init();
+            // Remplace le placeur de bateaux. Sinon, à mettre en commentaires.
+            DebugInit();
             Server.OpenServer();
+        }
+        static int lineCharacterToInt(char charToUnicode)
+        {
+            return (int)charToUnicode - (int)'A';
+        }
 
-            //creation des bateaux
-            string[,] battleMap = new string[10, 10];
-            Dictionary<string, int> boats = new Dictionary<string, int>();
+
+        public static void Init()
+        {
             boats.Add("carrier", 5);
             boats.Add("battleship", 4);
             boats.Add("cruiser", 3);
             boats.Add("submarine", 3);
             boats.Add("destroyer", 2);
+            playerHealth = boats.Sum(x => x.Value);
+        }
 
-            int playerHealth = boats.Sum(x => x.Value);
-
-            longitudeShot = 0;
-            latitudeShot = 0;
-
+        public static void DebugInit()
+        {
             battleMap[1, 1] = "carrier";
             battleMap[1, 2] = "carrier";
             battleMap[1, 3] = "carrier";
@@ -48,108 +62,81 @@ namespace ConsoleApplication1
             battleMap[7, 4] = "submarine";
             battleMap[8, 8] = "destroyer";
             battleMap[8, 9] = "destroyer";
+         
+        }
+        public static bool InputCheck()
+        {
+            linePosition = playerInput.Substring(0, 1/*EXCLU*/);
+            columnPosition = playerInput.Substring(1);
 
-            // Input check and crunch
-            string playerInput = "";
             Regex longitudeCheck = new Regex(@"^[A-J]+$");
             Regex latitudeCheck = new Regex(@"^[0-9]+$");
-
-            while (playerHealth > 0)
-            {
-                Console.WriteLine("Indiquez les coordonées du tir.");
-                playerInput = Console.ReadLine();
-
-
-                void CoordCheck(string input)
+           
+                // Verifier la validité de la ligne
+                if (!longitudeCheck.IsMatch(linePosition))
                 {
-                    try
-                    {
-                        string linePosition = input.Substring(0, 1/*EXCLU*/);
-                        string columnPosition = playerInput.Substring(1);
-
-                        // Verifier la validité de la ligne
-                        if (longitudeCheck.IsMatch(linePosition))
-                        {
-                            longitudeShot = lineCharacterToInt(playerInput[0]);
-                        }
-                        else
-                        {
-                            throw new Exception("La lettre de la ligne n'est pas valide");
-                        }
-                        // Verifier la validité de la colonne
-                        if (latitudeCheck.IsMatch(columnPosition)
-                            && int.Parse(columnPosition) > 0 && int.Parse(columnPosition) <= 10)
-                        {
-                            latitudeShot = int.Parse(columnPosition) - 1;
-                        }
-                        else
-                        {
-                            throw new Exception("Le chiffre de la colonne n'est pas valide");
-                        }
-
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine($"{ex.Message}");
-                    }
-                    Console.WriteLine($"{longitudeShot},{latitudeShot}");
+                    Console.WriteLine("La lettre de la ligne n'est pas valide");
+                    return false;
                 }
-                CoordCheck(playerInput);
-
-
-                // Attaque de l'adversaire
-                if (battleMap[longitudeShot, latitudeShot] == null)
+                // Verifier la validité de la colonne
+                if (!latitudeCheck.IsMatch(columnPosition)
+                    || int.Parse(columnPosition) <= 0 || int.Parse(columnPosition) > 10)
                 {
-                    Console.WriteLine("On a entendu plouf au loin. Et des rires distants.");
+                    Console.WriteLine("Le chiffre de la colonne n'est pas valide");
+                    return false;
                 }
-                else if (battleMap[longitudeShot, latitudeShot] == "Touché")
-                {
-                    Console.WriteLine($"Vous avez déjà tiré sur cette case. Achetez-vous des lunettes.");
-                }
-                else if (battleMap[longitudeShot, latitudeShot] != null)
-                {
-                    Console.WriteLine(battleMap[longitudeShot, latitudeShot]);
 
-                    Console.WriteLine($"Le bateau de type {battleMap[longitudeShot, latitudeShot].ToString()} a été touché en {playerInput}");
-                    boats[battleMap[longitudeShot, latitudeShot]] -= 1;
-                    if (boats[battleMap[longitudeShot, latitudeShot]] == 0)
-                    {
-                        battleMap[longitudeShot, latitudeShot] = "Touché";
-                        Console.WriteLine("Touché. Coulé.");
-                    }
-                    else
-                    {
-                        battleMap[longitudeShot, latitudeShot] = "Touché";
-                    }
-                    playerHealth--;
-                }
-            }
-
-            while (true)
-            {
-                Console.WriteLine("Fin de la partie. Voulez-vous rejouer? Ecrivez oui pour rejouer. Ecrivez non pour quitter.");
-                playerInput = Console.ReadLine();
-                try
-                {
-                    if (playerInput == "oui")
-                    {
-                        Console.Clear();
-                        Main(null);
-                    }
-                    if (playerInput == "non")
-                    {
-                        break;
-                    }
-                }
-                catch
-                {
-                    Console.WriteLine("Commande invalide");
-                }
-            }
+            return true;
         }
-        static int lineCharacterToInt(char charToUnicode)
+        public static string InputParser() 
         {
-            return (int)charToUnicode - (int)'A';
+            linePosition = playerInput.Substring(0, 1/*EXCLU*/);
+            columnPosition = playerInput.Substring(1);
+            longitudeShot = lineCharacterToInt(playerInput[0]);
+            latitudeShot = int.Parse(columnPosition) - 1;
+            message = CheckAtk(longitudeShot, latitudeShot);
+            return message;
+        }
+
+        public static string CheckAtk(int longitudeShot, int latitudeShot)
+        {
+            string message = "";
+            
+            // Attaque de l'adversaire
+            if (battleMap[longitudeShot, latitudeShot] == null)
+            {
+                message = "On a entendu plouf au loin. Et des rires distants.";
+                return message;
+                //Console.WriteLine("On a entendu plouf au loin. Et des rires distants.");
+            }
+            else if (battleMap[longitudeShot, latitudeShot] == "Touché")
+            {
+                message = $"Vous avez déjà tiré sur cette case. Achetez - vous des lunettes.";
+                return message;
+               // Console.WriteLine($"Vous avez déjà tiré sur cette case. Achetez-vous des lunettes.");
+            }
+            else if (battleMap[longitudeShot, latitudeShot] != null)
+            {
+                //Console.WriteLine(battleMap[longitudeShot, latitudeShot]);
+
+                //Console.WriteLine($"Le bateau de type {battleMap[longitudeShot, latitudeShot].ToString()} a été touché en {playerInput}");
+                boats[battleMap[longitudeShot, latitudeShot]] -= 1;
+                if (boats[battleMap[longitudeShot, latitudeShot]] == 0)
+                {
+                    battleMap[longitudeShot, latitudeShot] = "Touché";
+                    message = "Touché. Coulé.";
+                    return message;
+                    //Console.WriteLine("Touché. Coulé.");
+                }
+                else
+                {
+                    battleMap[longitudeShot, latitudeShot] = "Touché";
+                    message = "Touché";
+                    return message;                  
+                }
+                
+            }
+            return null;
         }
     }
 }
--- Bataille navale/Server.cs
+++ Bataille navale/Server.cs
@@ -18,52 +18,88 @@ namespace Bataille_navale
             {
                 // Création des variables contenant le port et l'adresse IP que le server doit écouter.
                 Int32 port = 13000;
-                IPAddress localAddr = IPAddress.Parse("192.168.1.67");
-
+                IPAddress localAddr = IPAddress.Parse("192.168.1.33");
                 // Création d'un objet TcpListener avec en paramétre le port et l'adresse IP
                 server = new TcpListener(localAddr, port);
-
-                // Ouverture du server.
+                // Ouverture du serveur.
                 server.Start();
-
                 // Création d'une variable bytes pour récupérer les données transmise et d'une variable string pour récupérer les données transcrit en string 
                 Byte[] bytes = new Byte[256];
                 String data = null;
-
-                // Lancement de la boucle d'écoute.
+                // Lancement de la boucle d'écoute 
                 while (true)
                 {
                     Console.Write("Waiting for a connection... ");
-
                     // Reception de la requête demande client envoyé..
                     TcpClient client = server.AcceptTcpClient();
                     Console.WriteLine("Connected!");
-
                     data = null;
-
                     // Lecture du contenu envoyé
                     NetworkStream stream = client.GetStream();
-
-                    int i;
-
-                    // Boucle sur le contenu envoyé.
-                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    int i = 0;
+                    // Boucle sur de jeu. Commence par la lecture d'une attaque par le client
+                    while (Program.playerHealth > 0)
                     {
+                        stream.Flush();
+                        while(i == 0)
+                        {
+                            i = stream.Read(bytes, 0, bytes.Length);
+                        }
+                        // 1 - Recevoir l'attaque ennemie
                         // transcrit les bytes envoyé en string.
                         data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                        Console.WriteLine("Received: {0}", data);
+                        // Parser l'attaque en coordonnées tableau
+                        Program.playerInput = data;
+                        Console.WriteLine("Le client a envoyé le message suivant : " + Program.playerInput);
+                        // Convertit l'attaque en coordonnées tableau, modifie le tableau et renvoie le message du résultat de l'attaque ennemi à envoyer au client
+                        string message = Program.InputParser();
+                        // 2 - Envoyer le message du résultat de l'attaque ennemi au client
+                        // Envoi du message au client
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(message);
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine("Le serveur envoie le message suivant : " + message);
 
-                        for (int msgNumber = 0; msgNumber < 2; msgNumber++) {
-                            Console.WriteLine("Indiquez votre attaque");
-                            data = Console.ReadLine();
-                            //transcrit le string en bytes
-                            byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
-
-                            // renvoie le message.
-                            stream.Write(msg, 0, msg.Length);
-                            Console.WriteLine("Sent: {0}", data);
+                        // 3 - Saisir notre attaque
+                        // Recevoir la saisie utilisateur
+                        // Boucle de vérification de validité de la saisie. Sinon, on revient à l'étape précédente
+                        do
+                        {
+                            Console.WriteLine("Indiquez les coordonées du tir.");
+                            Program.playerInput = Console.ReadLine();
                         }
-                        
+                        while (!Program.InputCheck());
+
+                        // 4 - Envoyer l'attaque à l'ennemi
+                        // Envoi du string au client
+
+                        message = Program.playerInput;
+                        msg = System.Text.Encoding.ASCII.GetBytes(message);
+                        stream.Write(msg, 0, msg.Length);
+                    }
+
+                    // Game over. On peux recommencer le jeu.
+                    while (true)
+                    {
+                        Console.WriteLine("Fin de la partie. Voulez-vous rejouer? Ecrivez oui pour rejouer. Ecrivez non pour quitter.");
+                        string playerInput = Console.ReadLine();
+                        try
+                        {
+                            if (playerInput == "oui")
+                            {
+                                Console.Clear();
+                                Program.Main(null);
+                            }
+                            if (playerInput == "non")
+                            {
+                                stream.Close();
+                                client.Close();
+                                break;
+                            }
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Commande invalide");
+                        }
                     }
 
                     // Shutdown and end connection
@@ -79,7 +115,6 @@ namespace Bataille_navale
                 // Stop listening for new clients.
                 server.Stop();
             }
-
             Console.WriteLine("\nHit enter to continue...");
             Console.Read();
         }
